764924d960a64cefb8b77f8b0901b7e19a9fe21e
KLY Plus & more <klyplusandmore@gmail.com>
2025-07-16 11:19:51 +0000

    is it because of a fnction?

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -58,15 +58,15 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {navItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                >
-                  <Link href={item.href} className="flex items-center gap-2">
+                <Link href={item.href}>
+                  <SidebarMenuButton
+                    isActive={pathname === item.href}
+                    tooltip={item.label}
+                  >
                     <item.icon />
                     <span>{item.label}</span>
-                  </Link>
-                </SidebarMenuButton>
+                  </SidebarMenuButton>
+                </Link>
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
